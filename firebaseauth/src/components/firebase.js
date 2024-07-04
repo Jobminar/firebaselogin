18420774029b72df56a2c006ab930da6a83f4c7b
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-07-04 10:31:04 +0530

    changed the firebase file

--- firebaseauth/src/components/firebase.js
+++ firebaseauth/src/components/firebase.js
@@ -3,15 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAxRvcsw5l8H8DrJZn6N245EbYfgg6Lowo",
-  authDomain: "coolie-38613.firebaseapp.com",
-  databaseURL:
-    "https://coolie-38613-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "coolie-38613",
-  storageBucket: "coolie-38613.appspot.com",
-  messagingSenderId: "851752918718",
-  appId: "1:851752918718:web:33ecc7f382126d989688ef",
-  measurementId: "G-5RTZD831FT",
+  apiKey: "AIzaSyBYXJ5jI1AwToNq4YaqJx5xrC1iQ-YR-uo",
+  authDomain: "coolie-9cf4f.firebaseapp.com",
+  projectId: "coolie-9cf4f",
+  storageBucket: "coolie-9cf4f.appspot.com",
+  messagingSenderId: "221342155571",
+  appId: "1:221342155571:web:aea14e8682c3057c5bb446",
+  measurementId: "G-92CSDLN4PJ",
 };
 
 // Initialize Firebase
